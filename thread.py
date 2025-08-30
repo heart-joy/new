92260d1038c7c2fc888b03b1ff4ffc4efaa25fc1
turt1e <125718914+heart-joy@users.noreply.github.com>
2025-08-30 21:21:05 +0800

    Add files via upload

---
+++ thread.py
@@ -0,0 +1,29 @@
+import threading
+import time
+from time import sleep
+
+class MyThread(threading.Thread):
+    def __init__(self, number, char):
+        threading.Thread.__init__(self)
+        self.number = number
+        self.char = char
+
+    def run(self):
+        print(f"starting thread {self.name}")
+        task(self.name, self.number, self.char)
+        print(f"exiting thread {self.name}")
+    
+def task(threadName, number, char):
+    while number > 0:
+        sleep(1)
+        current_time = time.strftime("%H:%M:%S", time.localtime())
+        print(f"[{current_time}]{threadName}: {char}")
+        number -= 1
+        
+thread1 = MyThread(4,'A')
+thread2 = MyThread(2,'B')
+
+thread1.start()
+thread2.start()
+
+sleep(3)
