971af6f098486cf770fa6c31902b6344ad00eb82
heart-joy <137769624@qq.com>
2025-08-31 11:01:24 +0800

    update

--- classlibrary.py
+++ classlibrary.py
@@ -5,5 +5,21 @@ class MyThread(threading.Thread):
 
     def run(self):
         print(f"starting thread {self.name}")
-        task(self.name, self.number, self.char)
-        print(f"exiting thread {self.name}")
+        lock.acquire()
+        task(self.name, self.number)
+        lock.release()
+
+def task(threadName, number):
+    while number > 0:
+        sleep(1)
+        current_time = time.strftime("%H:%M:%S", time.localtime())
+        print(f"[{current_time}]{threadName}: {number}/n")
+        number -= 1
+    print(f"exiting thread {threadName}")
+    
+lock = threading.Lock()
+data_list = [0,0,0,0]
+
+thread1 = MyThread(1)
+thread2 = MyThread(2)
+thread3 = MyThread(3)
