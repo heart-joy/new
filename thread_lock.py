9e0e3dbf3823ab5424f44be4492228c914260a17
heart-joy <137769624@qq.com>
2025-08-31 11:39:40 +0800

    update:lock

--- thread_lock.py
+++ thread_lock.py
@@ -1,13 +1,28 @@
 import threading
 import time
 from time import sleep
-from classlibrary import MyThread
 
+
+class MyThread(threading.Thread):
+    def __init__(self, number):
+        threading.Thread.__init__(self)
+        self.number = number
+
+    def run(self):
+        print(f"starting thread {self.name}")
+        lock.acquire()
+        task(self.name, self.number)
+        lock.release()
+
+    def _del_(self):
+        print(f"exiting thread {self.name}")
+        
 def task(threadName, number):
     while number > 0:
         sleep(1)
+        data_list[number-1] += 1
         current_time = time.strftime("%H:%M:%S", time.localtime())
-        print(f"[{current_time}]{threadName}: {number}/n")
+        print(f"[{current_time}]{threadName}: {number}\n")
         number -= 1
     print(f"thread {threadName} finished")
     
@@ -18,3 +33,12 @@ thread1 = MyThread(1)
 thread2 = MyThread(2)
 thread3 = MyThread(3)
 
+thread1.start()
+thread2.start()
+thread3.start() 
+
+thread1.join()
+thread2.join()
+thread3.join()
+
+print("all threads finished")
