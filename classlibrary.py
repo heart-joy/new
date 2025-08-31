62517b4730ddab8d2ded55cad28f60274839c654
heart-joy <137769624@qq.com>
2025-08-31 11:02:21 +0800

    fix:update erro

--- classlibrary.py
+++ classlibrary.py
@@ -9,17 +9,3 @@ class MyThread(threading.Thread):
         task(self.name, self.number)
         lock.release()
 
-def task(threadName, number):
-    while number > 0:
-        sleep(1)
-        current_time = time.strftime("%H:%M:%S", time.localtime())
-        print(f"[{current_time}]{threadName}: {number}/n")
-        number -= 1
-    print(f"exiting thread {threadName}")
-    
-lock = threading.Lock()
-data_list = [0,0,0,0]
-
-thread1 = MyThread(1)
-thread2 = MyThread(2)
-thread3 = MyThread(3)
