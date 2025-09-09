a30d1853113632abd5b03f66c17edf01d0900fb3
heart-joy <137769624@qq.com>
2025-09-09 21:14:50 +0800

    update:RLock

---
+++ locker/rlock.py
@@ -0,0 +1,34 @@
+import threading
+
+lock = threading.RLock()
+
+def add():
+    global num
+    for i in range(1000000):
+      lock.acquire()
+      lock.acquire()
+      num += 1
+      lock.release()
+      lock.release()
+
+def sub():
+    global num
+    for i in range(1000000):
+      lock.acquire()
+      lock.acquire()
+      num -= 1
+      lock.release()
+      lock.release()
+
+if __name__ == "__main__":
+    num = 0
+    t1 = threading.Thread(target=add)
+    t2 = threading.Thread(target=sub)
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    print(num)
